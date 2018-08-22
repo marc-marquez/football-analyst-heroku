af74abe48c0b08676835154883dd45a3b8cf90fe
Marc Marquez <marc.marquez@gmail.com>
2018-08-21 22:25:53 -0700

    Changed to pbp_2017_bare_min_merged collection without using aggregate for faster db retrieval.

--- FootballAnalyst.py
+++ FootballAnalyst.py
@@ -15,8 +15,8 @@ MONGO_URI = os.getenv('MONGODB_URI','mongodb://ds129010.mlab.com:29010')
 DBS_NAME = os.getenv('MONGO_DB_NAME','heroku_51gxnjkc')
 #PBP_2017_COLLECTION_NAME = 'pbp_2017'
 #PBP_2017_COLLECTION_NAME = 'pbp_2017_pass_run_only'
-PBP_2017_COLLECTION_NAME = 'pbp_2017_pass_run_bare_min'
-ROSTERS_2017_COLLECTION_NAME = 'rosters_2017'
+PBP_2017_COLLECTION_NAME = 'pbp_2017_pass_run_bare_min_merged'
+#ROSTERS_2017_COLLECTION_NAME = 'rosters_2017'
 
 
 @app.route("/")
@@ -39,6 +39,9 @@ def nfl_data():
     #    '_id': False, 'GameID': True, 'Date': True, 'Week':True, 'posteam': True, 'DefensiveTeam': True,'YardsGained': True, 'PassAttempt': True, 'PassLocation': True, 'PassOutcome': True,
     #    'Receiver_ID': True
     #}
+    PBP_FIELDS = {
+        '_id': False, 'RunLocation': True, 'PassOutcome': True, 'down': True, 'PlayType': True, 'YardsGained': True, 'qtr': True, 'Position': True, 'posteam': True, 'GameID': True, 'PassLength': True, 'RunGap': True, 'PassLocation': True, 'Week': True
+    }
     #PLAYER_FIELDS = {
     #    '_id': False, 'GSIS_ID': True, 'Pos': True
     #}
@@ -52,68 +55,68 @@ def nfl_data():
     with MongoClient(MONGO_URI) as conn:
         # Define which collection we wish to access
         pbp_2017_collection = conn[DBS_NAME][PBP_2017_COLLECTION_NAME]
-        rosters_2017_collection = conn[DBS_NAME][ROSTERS_2017_COLLECTION_NAME]
+        #rosters_2017_collection = conn[DBS_NAME][ROSTERS_2017_COLLECTION_NAME]
 
         # Retrieve a result set only with the fields defined in FIELDS
         # and limit the the results to 55000
-        #pbp_2017 = pbp_2017_collection.find(projection=PBP_FIELDS, limit=100000)
+        pbp_2017 = pbp_2017_collection.find(projection=PBP_FIELDS, limit=100000)
         #rosters_2017 = rosters_2017_collection.find(projection=PLAYER_FIELDS, limit=100000)
 
         #merge collections together based on Receiver_ID and GSIS_ID
 
-        pipeline = [
-            {
-                "$match":
-                {
-                    #"PassAttempt": 1,
-                    #"PlayAttempted":1
-                    #"RushAttempt":1
-                    "$or": [{"PlayType":"Pass"},{"PlayType":"Run"}]
-                    #"PlayType": "Pass"
-                }
-            },
-            {
-                "$lookup":
-                {
-                    "from": "rosters_2017",
-                    "localField": "Receiver_ID",
-                    "foreignField": "GSIS_ID",
-                    "as": "receiverInfo"
-                }
-            },
-            {
-                "$unwind":"$receiverInfo"
-            },
-            {
-                "$project":
-                {
-                    "_id": 0,
-                    "GameID": 1,
-                    "posteam":1,
-                    #"Date":1,
-                    "Week":1,
-                    #"TimeUnder":1,
-                    #"DefensiveTeam":1,
-                    "YardsGained":1,
-                    #"PlayAttempted":1,
-                    #"PassAttempt":1,
-                    #"RushAttempt":1,
-                    "PassLocation":1,
-                    "PassOutcome":1,
-                    "RunLocation":1,
-                    "RunGap":1,
-                    "down":1,
-                    "qtr":1,
-                    "PassLength":1,
-                    "PlayType":1,
-                    #"Location":{"$or": [{"PassLocation"},{"RunLocation"}]},
-                    "Position":"$receiverInfo.Pos"
-                }
-            },
-            {
-                "$limit": 35000
-            },
-        ]
+        #pipeline = [
+        #    {
+        #        "$match":
+        #        {
+        #            #"PassAttempt": 1,
+        #            #"PlayAttempted":1
+        #            #"RushAttempt":1
+        #            "$or": [{"PlayType":"Pass"},{"PlayType":"Run"}]
+        #            #"PlayType": "Pass"
+        #        }
+        #    },
+        #    {
+        #        "$lookup":
+        #        {
+        #            "from": "rosters_2017",
+        #            "localField": "Receiver_ID",
+        #            "foreignField": "GSIS_ID",
+        #            "as": "receiverInfo"
+        #        }
+        #    },
+        #    {
+        #        "$unwind":"$receiverInfo"
+        #    },
+        #    {
+        #        "$project":
+        #        {
+        #            "_id": 0,
+        #            "GameID": 1,
+        #            "posteam":1,
+        #            #"Date":1,
+        #            "Week":1,
+        #            #"TimeUnder":1,
+        #            #"DefensiveTeam":1,
+        #            "YardsGained":1,
+        #            #"PlayAttempted":1,
+        #            #"PassAttempt":1,
+        #            #"RushAttempt":1,
+        #            "PassLocation":1,
+        #            "PassOutcome":1,
+        #            "RunLocation":1,
+        #            "RunGap":1,
+        #            "down":1,
+        #            "qtr":1,
+        #            "PassLength":1,
+        #            "PlayType":1,
+        #            #"Location":{"$or": [{"PassLocation"},{"RunLocation"}]},
+        #            "Position":"$receiverInfo.Pos"
+        #        }
+        #    },
+        #    {
+        #        "$limit": 35000
+        #    },
+        #]
 
         #console.log(all_2017)
         #combo_2017 = pbp_2017_collection.find(projection=COMBO_FIELDS, limit=100000)
@@ -121,7 +124,7 @@ def nfl_data():
         # Convert projects to a list in a JSON object and return the JSON data
         #pbp_2017 = pbp_2017_collection.find({}, limit=100000)
 
-        pbp_2017 = pbp_2017_collection.aggregate(pipeline)
+        #pbp_2017 = pbp_2017_collection.aggregate(pipeline)
         #pprint(pbp_2017)
         #print pbp_2017
         #print(list(pbp_2017))
